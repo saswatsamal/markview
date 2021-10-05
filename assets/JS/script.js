1171625e000599898cb7884efaacee5743d1c72a
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-10-05 02:01:59 +0000

    fix issue with table rendering

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,9 @@
 const textEditor = document.querySelector('#editor');
 const textPreview = document.querySelector('#preview');
+const converter = new showdown.Converter({tables: true, tasklists: true, smoothLivePreview: true})
 
 textEditor.addEventListener('keyup', evt => {
   const { value } = evt.target;
-  var converter = new showdown.Converter(),
-    html = converter.makeHtml(value);
+  html = converter.makeHtml(value);
   textPreview.innerHTML = html;
 })
